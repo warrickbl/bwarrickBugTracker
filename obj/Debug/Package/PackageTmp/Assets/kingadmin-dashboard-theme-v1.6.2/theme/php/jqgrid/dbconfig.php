3d3d439ea7f8bfacbed5d8196cd4fe224cd4bcaa
Bri Warrick <30389772+warrickbl@users.noreply.github.com>
2017-10-11 10:22:38 -0400

    before updating NuGet packages

--- obj/Debug/Package/PackageTmp/Assets/kingadmin-dashboard-theme-v1.6.2/theme/php/jqgrid/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$DB_HOST = 'localhost';
-$DB_NAME = 'dashboard_demo';
-$DB_USERNAME = 'root';
-$DB_PASSWORD = 'root';
-
-?>
--- obj/Debug/Package/PackageTmp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bwarrickBugTracker.MvcApplication" Language="C#" %>
